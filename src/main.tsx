7ec8d2d29a5778273aacf08da6066a655e56e9c2
reaganotema <reaganotema2022@gmail.com>
2026-03-12 08:49:21 -0700

    updates

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,27 @@
+/**
+ * NextERP Systems
+ * Enterprise Management Ecosystem
+ * Developed by Reagan Otema & Binsobedde Najiib
+ */
+
+import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import App from "./App";
 import "./globals.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+// Get root container
+const container = document.getElementById("root");
+
+if (!container) {
+  throw new Error("Root container missing in index.html");
+}
+
+// Create React root
+const root = createRoot(container);
+
+// Render application
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
